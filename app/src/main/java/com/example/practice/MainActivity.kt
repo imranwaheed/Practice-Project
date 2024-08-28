d68422fd95fbc7cbfef38b0b65383d372dae52a9
Muhammad Imran <m.imran0037@gmail.com>
2024-08-28 01:04:31 -0700

    Web view URL changed to my github profile

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -1,6 +1,8 @@
   package com.example.practice
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,6 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gotonextbtn = findViewById<TextView>(R.id.gotonextbtn)
+        gotonextbtn.setOnClickListener {
+            intent = Intent(this@MainActivity ,SecondActivity ::class.java)
+            startActivity(intent)
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/practice/SecondActivity.kt
@@ -0,0 +1,23 @@
+package com.example.practice
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val webView = findViewById<WebView>(R.id.webview)
+
+        webView.webViewClient= WebViewClient()
+
+        webView.loadUrl("https://github.com/imranwaheed")
+
+
+
+    }
+}
